f9d0df2c772304ad6629b3cd2412dd7085c71396
André Navarro <andrelfnavarro@gmail.com>
2019-05-10 15:03:57 -0300

    feature/add-user w/Loader and fixed

---
+++ src/components/Loader.tsx
@@ -0,0 +1,17 @@
+import { ClipLoader } from 'react-spinners';
+import React from 'react';
+
+
+
+export default function Loader (loading:boolean){
+    return (
+        <div className='sweet-loading' style={{ textAlign: 'center', display: 'block' }}>
+            <ClipLoader
+                sizeUnit={"px"}
+                size={150}
+                color={'#e6b3ff'}
+                loading={loading}
+              />
+        </div>
+    )
+}
